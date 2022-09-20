48683869a65743fe0c1a5998cd7125789d8b0ab4
Jacob <yo.yashkins@gmail.com>
2022-09-20 19:33:32 +0700

    Отредактировал EX009

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -4,9 +4,9 @@
     if(arg2 > result) result = arg2;
     if(arg3 > result) result = arg3;
     return result;
-}//             0   1   2   3   4   5   6   7   8
-int[] array = { 11, 21, 31, 412, 15, 61, 17, 18, 19 };
-array[0] = 1;
+}
+    //             0   1   2   3   4   5   6   7   8
+int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 191 };
 
 int result = Max(
     Max(array[0], array[1], array[2]),
